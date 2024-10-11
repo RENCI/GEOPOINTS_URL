2c7e67f8c41db112664de38f44aa18ad29fb3c16
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-11 10:34:19 -0400

    implementing test framework

--- test/test_geopoints_url.py
+++ test/test_geopoints_url.py
@@ -14,6 +14,10 @@ def test_geopoints_url():
     """
 
     # initialize the input arguments
+    # --url "https://apsviz-thredds-dev.apps.renci.org/thredds/dodsC/2023/gfs/2023052300/wnat_53k_v1.0/sapelo2/adcirc_gfs_53k/gfsforecast/fort.63.nc" --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
+    # --url 'http://tdsres.apps.renci.org/thredds/dodsC/datalayers/test/fort.63.nc' --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
+    # --url 'http://tdsres.apps.renci.org/thredds/dodsC/datalayers/test/fort.63.d0.no-unlim.T.nc' --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
+    # --url 'http://tdsres.apps.renci.org/thredds/dodsC/datalayers/test/fort.63.d0.no-unlim.T.rc.nc' --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
 
     # call the function, check the return
     ret_val = True
