57fa040bc15974c95cc419d1c12951524f0de0d2
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-11 10:23:28 -0400

    adding test files

---
+++ test/test_generate_urls_from_times.py
@@ -0,0 +1,11 @@
+"""
+    Test generate_urls_from_times - Tests generate urls from times functionality
+
+    Authors: Dr Jeff Tilson, Phil Owen @RENCI.org
+"""
+
+import generate_urls_from_times
+
+
+def test_generate_urls_from_times():
+    pass
---
+++ test/test_geopoints_url.py
@@ -0,0 +1,11 @@
+"""
+    Test geopoints_url - Tests geopoints url functionality
+
+    Authors: Dr. Jeff Tilson, Phil Owen @RENCI.org
+"""
+
+import geopoints_url
+
+
+def test_geopoints_url():
+    pass
