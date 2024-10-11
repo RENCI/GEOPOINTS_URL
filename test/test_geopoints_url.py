90d48724b165209f8eb346084bf2b56382dee405
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-11 11:17:40 -0400

    adding in a better call to test

--- test/test_geopoints_url.py
+++ test/test_geopoints_url.py
@@ -14,10 +14,15 @@ def test_geopoints_url():
 
     """
     # example inputs
-    # --url "https://apsviz-thredds-dev.apps.renci.org/thredds/dodsC/2023/gfs/2023052300/wnat_53k_v1.0/sapelo2/adcirc_gfs_53k/gfsforecast/fort.63.nc" --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
+
+    # --url "https://apsviz-thredds-dev.apps.renci.org/thredds/dodsC/2023/gfs/2023052300/wnat_53k_v1.0/sapelo2/adcirc_gfs_53k/gfsforecast/fort.63.nc"
+    # --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
+
     # --url 'https://tdsres.apps.renci.org/thredds/dodsC/datalayers/test/fort.63.nc' --variable_name 'zeta' --lon -79.6725155674 --lat 32.8596518752
+
     # --url 'https://tdsres.apps.renci.org/thredds/dodsC/datalayers/test/fort.63.d0.no-unlim.T.nc' --variable_name 'zeta' --lon -79.6725155674
     # --lat 32.8596518752
+
     # --url 'https://tdsres.apps.renci.org/thredds/dodsC/datalayers/test/fort.63.d0.no-unlim.T.rc.nc' --variable_name 'zeta' --lon -79.6725155674
     # --lat 32.8596518752
 
