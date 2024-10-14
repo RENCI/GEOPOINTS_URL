63aa13b989b5df94b85bace56f411ff610272fea
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-14 15:56:09 -0400

    fixing the url to the data, making the input params match the order of the code.

--- test/test_geopoints_url.py
+++ test/test_geopoints_url.py
@@ -22,19 +22,22 @@ def test_geopoints_url():
 
     # create a list of test urls
     urls: list = [
-        # the test URL below is expected to point to the TDS-res in the prod namespace.
-        # if running in k8s, the TDS service name can be used.
-        tds_svr + "thredds/dodsC/2023/al4/16/NCSC_SAB_v1.23/ht-ncfs.renci.org/ncsc123_nhc_al042023/ofcl/fort.63.nc",
+            # the test URL below is expected to point to the TDS-res in the prod namespace.
+            # if running in k8s, the TDS service name can be used.
+            # tds_svr + 'thredds/dodsC/Datalayers/test/fort.63.nc'
+
+            # the dev TDS server should be used for this endpoint
+            tds_svr + "thredds/dodsC/2023/al4/16/NCSC_SAB_v1.23/ht-ncfs.renci.org/ncsc123_nhc_al042023/ofcl/fort.63.nc",
         ]
 
     # create a named tuple for the args to mimic the input
     argsNT = namedtuple('argsNT', ['lon', 'lat', 'variable_name', 'kmax', 'alt_urlsource', 'url',
-                                   'keep_headers', 'ndays', 'ensemble'])
+                                   'keep_headers', 'ensemble', 'ndays'])
 
     # for each test url
     for url in urls:
         # init the named tuple for the call
-        args = argsNT(-79.6725155674, 32.8596518752, 'zeta', 10, None, url, True, 0, None)
+        args = argsNT(-79.6725155674, 32.8596518752, 'zeta', 10, None, url, True, None, 0)
 
         # call the function, check the return
         ret_val = gu.main(args)
